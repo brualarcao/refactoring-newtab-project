db2c7560baad8944ee8168902d18f862a9708aca
brualarcao <brunno.cesar@live.com>
2022-01-23 17:49:16 -0300

    Updates

---
+++ precache-manifest.4553fcbc26e0c047d57a98383d61ba24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d3f233eb928e477e1eb291e0fd4c9f",
+    "url": "/refactoring-newtab-project/index.html"
+  },
+  {
+    "revision": "fff4b393b3ed231361e1",
+    "url": "/refactoring-newtab-project/static/css/main.3e993436.chunk.css"
+  },
+  {
+    "revision": "10ae0603604359519bfb",
+    "url": "/refactoring-newtab-project/static/js/2.50c414a1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/refactoring-newtab-project/static/js/2.50c414a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff4b393b3ed231361e1",
+    "url": "/refactoring-newtab-project/static/js/main.986a220b.chunk.js"
+  },
+  {
+    "revision": "31bf5ab2acbb08b48b5f",
+    "url": "/refactoring-newtab-project/static/js/runtime-main.8362fd8a.js"
+  }
+]);
